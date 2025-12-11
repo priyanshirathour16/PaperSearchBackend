5085d1c06e675af8767da1a2e670d41c957fea8e
priyanshi rathour <priyanshirathour@gmail.com>
2025-12-11 19:50:52 +0530

    stage changes

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 const serverless = require("serverless-http");
-const express = require('express');
-const app = express();
-// const app = require("./src/index.js");
-const PORT = process.env.PORT || 5000;
+const app = require("../src/index.js");
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-// module.exports = serverless(app);
+module.exports = serverless(app);
