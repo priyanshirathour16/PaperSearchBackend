2ab18d371cfcc9cf47fd7c67c76ed7025862ecf3
priyanshi rathour <priyanshirathour@gmail.com>
2025-12-11 19:46:51 +0530

    express connection

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const serverless = require("serverless-http");
-const app = require("./src/index.js");
+const express = require('express');
+const app = express();
+// const app = require("./src/index.js");
+const PORT = process.env.PORT || 5000;
 
-module.exports = serverless(app);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+// module.exports = serverless(app);
