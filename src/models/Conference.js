9a51b34d4d2ae7f3f92d5454d18ffbd174136a52
priyanshi rathour <priyanshirathour@gmail.com>
2025-12-23 22:21:24 +0530

    conference logo added

--- src/models/Conference.js
+++ src/models/Conference.js
@@ -5,6 +5,14 @@ const Conference = sequelize.define('Conference', {
     name: {
         type: DataTypes.STRING,
         allowNull: false,
+    },
+    organized_by: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    start_date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
     }
 }, {
     paranoid: true, // Enable soft delete
